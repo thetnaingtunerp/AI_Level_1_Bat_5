a0613aa975a3214e781ae51367853e010d6e1c2f
ThetNaingTun <67203393+thetnaingtunerp@users.noreply.github.com>
2025-11-29 15:38:55 +0630

    first commit

---
+++ pythonlesson/lesson1.py
@@ -0,0 +1,8 @@
+# name = 'mg mg' , 'aung aung'
+
+list = ['mg mg' , 'aung aung' , 'kyaw kyaw', 30,20]
+set = {'mg mg' , 'aung aung' , 'kyaw kyaw', 30,20}
+tup = ('mg mg' , 'aung aung' , 'kyaw kyaw', 30,20)
+dic = {'key': 'values', 'name':'mg mg', 'address':'yangon'}
+
+
